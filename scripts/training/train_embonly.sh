de6524a2f5fe992db29b48ca7b6ea2b84406399b
lucaswychan <chanconan@gmail.com>
2025-08-21 21:51:55 +0800

    update the torch arch list

--- scripts/training/train_embonly.sh
+++ scripts/training/train_embonly.sh
@@ -12,10 +12,11 @@
 ######################
 ### Set enviroment ###
 ######################
-cd /home/wychanbu/gritlm/gritlm
-source /home/wychanbu/gritlm/.gritvenv/bin/activate
+cd /home/wychanbu/gritlm-own/gritlm
+source /home/wychanbu/gritlm-own/.gritvenv_old/bin/activate
 # export WANDB_PROJECT="gritlm"
-export CUDA_VISIBLE_DEVICES=0,1,3,4
+export CUDA_VISIBLE_DEVICES=3,5,6,7
+export TORCH_CUDA_ARCH_LIST="8.9"
 export HF_HOME=/data/wychanbu/huggingface
 export NCCL_P2P_DISABLE=1
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
@@ -23,10 +24,10 @@ export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 GPUS_PER_NODE=4
 
 LAUNCHER="accelerate launch \
-    --config_file /home/wychanbu/gritlm/scripts/configs/config_8gpusfsdp_qwen.yml \
+    --config_file /home/wychanbu/gritlm-own/scripts/configs/config_8gpusfsdp_qwen.yml \
     --num_machines 1 \
     --num_processes $GPUS_PER_NODE \
-    --main_process_port 8000 \
+    --main_process_port 6000 \
     --machine_rank 0 \
     --role localhost: \
     --tee 1 \
@@ -36,16 +37,16 @@ TRAIN_DATA=/data/wychanbu/re_data/hard-neg # replace with the directory of your 
 
 export CMD=" \
     -m training.run \
-    --output_dir /data/wychanbu/re_models/Nemotron-Research-Reasoning-Qwen-1.5B_hard_neg_no_lora/ \
-    --model_name_or_path nvidia/Nemotron-Research-Reasoning-Qwen-1.5B \
+    --output_dir /data/wychanbu/re_models/Qwen3-4B_embeddings/ \
+    --model_name_or_path Qwen/Qwen3-4B \
     --train_data $TRAIN_DATA \
     --learning_rate 2e-5 \
     --weight_decay 0.05 \
     --lr_scheduler_type cosine \
     --warmup_ratio 0.03 \
     --num_train_epochs 1 \
-    --per_device_train_batch_size 256 \
-    --gradient_accumulation_steps 2 \
+    --per_device_train_batch_size 64 \
+    --gradient_accumulation_steps 8 \
     --dataloader_drop_last \
     --normalized \
     --temperature 0.02 \
@@ -60,7 +61,7 @@ export CMD=" \
     --use_unique_indices \
     --attn bbcc \
     --gradient_checkpointing \
-    --attn_implementation sdpa \
+    --attn_implementation flash_attention_2 \
     --save_steps 200 \
     "
 
